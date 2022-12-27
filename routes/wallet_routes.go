28d6ca920a1c02ed6a0a18e2b65d59495086bc22
AzizRahimov <aziz.rahimov0001@gmail.com>
2022-12-27 17:46:57 +0500

    realised routes of Wallet

---
+++ routes/wallet_routes.go
@@ -0,0 +1,24 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+type PostRouteController struct {
+	postController controllers.PostController
+}
+
+type WalletRouteController struct {
+	walletRouteController controllers.WalletRouteController
+}
+
+func NewWalletControllerRoute(walletController controllers.WalletRouteController) WalletRouteController {
+	return WalletRouteController{walletController}
+
+}
+func (r *WalletRouteController) WalletRoute(rg *gin.RouterGroup) {
+	router := rg.Group("/wallet")
+
+	router.POST("/check_account", controller.CheckAccount)
+	router.POST("/top_up", controller.TopUp)
+	router.POST("/trn_history", controller.TrnHistory)
+	router.POST("/get_balance", controller.GetBalance)
+}
